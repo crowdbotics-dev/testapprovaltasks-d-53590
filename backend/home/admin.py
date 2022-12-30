e0932d8fbaff121603837c55434d6252610a56d9
crowdbotics-dev <engineering@crowdbotics.com>
2022-12-30 09:16:53 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Fn
+admin.site.register(Fn)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Fn(models.Model):
+    'Generated Model'
+    qwewrty = models.BigIntegerField()
